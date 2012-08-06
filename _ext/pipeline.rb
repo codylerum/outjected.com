950e7bc630ab34448e6efb1128bf82835714e5f5
Cody Lerum <cody.lerum@gmail.com>
2012-08-06 10:27:46 -0600

    add tagging and paginator

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,8 +6,11 @@ Awestruct::Extensions::Pipeline.new do
   helper UtilHelper
   extension Awestruct::Extensions::Disqus.new()
   extension Awestruct::Extensions::Posts.new( '/blog', :blogs )
-  extension Awestruct::Extensions::Atomizer.new( :blogs, '/blog/feed.atom', :feed_title=>'Outjected Blog', :content_url=> "/blog.html" )
   extension BlogsMap.new
+  extension Awestruct::Extensions::Paginator.new( :blogs, '/index', :per_page=>5 )
+  extension Awestruct::Extensions::Tagger.new( :blogs, '/index', '/blog/tags', :per_page=>5 )
+  extension Awestruct::Extensions::TagCloud.new( :blogs, '/blog/tags/index.xhtml' )
+  extension Awestruct::Extensions::Atomizer.new( :blogs, '/blog/feed.atom', :feed_title=>'Outjected Blog', :content_url=> "/blog.html" )
   extension Awestruct::Extensions::Sitemap.new()
 end
 
